2613227555e90795ce712b9ef54eaf4ae63a56fd
Md.Johirule <johirulislam574206@gmail.comm>
2023-05-08 11:18:36 +0600

    change header

--- components/Header.js
+++ components/Header.js
@@ -19,21 +19,6 @@ import { AiOutlineHeart } from "react-icons/ai";
 import classNames from "classnames";
 
 const Header = () => {
-<<<<<<< HEAD
-  const { LOGOUT, setUser } = useContext(RootContext);
-
-  const HandleLogOut = () => {
-    LOGOUT()
-      .then((res) => {
-        setUser(null);
-        toast.success("Logout success");
-      })
-      .catch((err) => {
-        console.log(err);
-        toast.error(err.data?.message);
-      });
-  };
-=======
   const { LOGOUT, setUser } = useContext(RootContext)
 
   const HandleLogOut = () => {
@@ -43,8 +28,7 @@ const Header = () => {
         toast.success('Logout success')
       })
       .catch(err => { console.log(err); toast.error(err.data?.message) })
->>>>>>> johir/Transport
-
+  }
   const buttonStyle = classNames(
     "border border-blue-500 p-2 rounded-full hover:bg-blue-500 hover:text-white transition duration-200 ease-linear hover:shadow-lg hover:cursor-pointer"
   );
