b700020936b5f76aecddab79dec66f8429d5454d
Md.Johirule <johirulislam574206@gmail.comm>
2023-05-11 21:54:11 +0600

    location route eidite

--- components/Destinations/DestinationProduct.js
+++ components/Destinations/DestinationProduct.js
@@ -26,19 +26,21 @@ const DestinationProduct = ({ singelLocation }) => {
 
   // ------dynamic area hotel get--------
   useEffect(() => {
+
+    if (!cetagoryId) return
+
     fetch(`https://travel-xone-server.vercel.app/api/v1/hotels?placeID=${cetagoryId}`)
       .then(res => res.json())
       .then(data => {
         sethotelAll(data.data)
         setLoding(false)
 
-
       })
   }, [cetagoryId]);
 
   // cetagory hotel place name
   useEffect(() => {
-    fetch(`https://travel-xone-server.vercel.app/api/v1/hotels/`)
+    fetch(`https://travel-xone-server.vercel.app/api/v1/hotels`)
       .then(res => res.json())
       .then(data => {
         sethotelCetagory(data?.data)
@@ -47,15 +49,15 @@ const DestinationProduct = ({ singelLocation }) => {
     Aos.init();
   }, [])
 
-  // if (loding) {
-  //   return <>
-  //     <div className="w-[100%] h-[100vh] justify-center flex items-center">
-  //       <div className="fixed top-0 right-0 h-screen w-screen z-50 flex justify-center items-center bg-[#0000004d]">
-  //         <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-4 border-blue-700"></div>
-  //       </div>
-  //     </div>
-  //   </>
-  // }
+  if (loding) {
+    return <>
+      <div className="w-[100%] h-[100vh] justify-center flex items-center">
+        <div className="fixed top-0 right-0 h-screen w-screen z-50 flex justify-center items-center bg-[#0000004d]">
+          <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-4 border-blue-700"></div>
+        </div>
+      </div>
+    </>
+  }
 
 
 
